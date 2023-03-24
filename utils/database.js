427700547da69dc3d0f4477591a8b64286c96071
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-03-24 14:47:01 +0200

    Set testing environment

--- utils/database.js
+++ utils/database.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+let DB_NAME;
+
+if (process.env.NODE_ENV.trim() === "testing") {
+  DB_NAME = process.env.DB_NAME_TESTING;
+} else {
+  DB_NAME = process.env.DB_NAME;
+}
+
 const HOST = process.env.DB_HOST;
-const NAME = process.env.DB_NAME;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-const sequelize = new Sequelize(NAME, USERNAME, PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, USERNAME, PASSWORD, {
   host: HOST,
   dialect: "mysql",
   logging: false,
