2836f84ce1b47f06815d345c976400a802e562d2
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-03-24 10:23:04 +0200

    Add env to gitignore

--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize("task_db", "user2", "hRMyzHVgtWgDJAwe", {
+  host: "18.157.127.87",
+  dialect: "mysql",
+});
 
 export default sequelize;
